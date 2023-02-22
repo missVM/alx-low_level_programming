2fe2c4a7805d39dd81d5ccf57c0586302cd47890
missVM <125476856+missVM@users.noreply.github.com>
2023-02-21 22:56:04 -0800

    print statement with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main . A program that prits a line using printf function
- * Return: 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return(0);
+printf("with proper grammar. but the outcome is a piece of art,\n);
+return (0);
 }
